fd9c95580a6e0622de8eecba7768d4affbd13e83
welson.santos <welsoncmp@gmail.com>
2024-02-05 21:49:59 -0300

    Título: Melhoria na interface do usuário e prevenção de overflow na calculadora
    Descrição:
    Este commit traz várias melhorias para o aplicativo de calculadora. As seguintes alterações foram feitas:
    
    1. Adicionado um `LayoutBuilder` para obter as restrições de layout do widget pai.
    2. A quantidade máxima de dígitos que podem ser inseridos agora é determinada pela largura da tela.
    3. Adicionada uma verificação no método `calcular` para remover dígitos extras se o número exceder o número máximo de dígitos permitidos.
    4. Imagens foram adicionadas para os botões que representam as operações matemáticas, melhorando a interface do usuário.
    
    Essas alterações garantem que o aplicativo de calculadora se adapte dinamicamente ao tamanho da tela, evite problemas de overflow e forneça uma experiência de usuário aprimorada com uma interface visualmente agradável.

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,31 @@
+// Importando o pacote Flutter Material Design.
 import 'package:flutter/material.dart';
 
+// Função principal que executa o aplicativo.
 void main() {
   runApp(
-    MaterialApp(
-      home: Calculadora(),
-      debugShowCheckedModeBanner: false,
-    )
+      MaterialApp(
+        home: Calculadora(), // Define a tela inicial do aplicativo.
+        debugShowCheckedModeBanner: false, // Remove a faixa de depuração.
+      )
   );
 }
 
-
+// Widget Calculadora que é um StatefulWidget, o que significa que mantém o estado.
 class Calculadora extends StatefulWidget {
   @override
-  State<Calculadora> createState() => _CalculadoraState();
+  State<Calculadora> createState() => _CalculadoraState(); // Cria o estado para o widget Calculadora.
 }
 
+// Classe de estado para o widget Calculadora.
 class _CalculadoraState extends State<Calculadora> {
-  String numero = '';
-  double a = 0.0;
-  String operacao = '';
-  double resultado = 0.0;
+  String numero = ''; // Armazena o número atualmente exibido.
+  double a = 0.0; // Armazena o primeiro número para cálculos.
+  String operacao = ''; // Armazena a operação a ser realizada.
+  double resultado = 0.0; // Armazena o resultado do cálculo.
+  int maxDigits = 10; // Armazena o número máximo de dígitos que podem ser inseridos.
 
+  // Função para realizar cálculos com base na tecla pressionada.
   void calcular(String tecla){
     switch(tecla){
       case '0':
@@ -37,6 +42,12 @@ class _CalculadoraState extends State<Calculadora> {
         setState(() {
           numero += tecla;
           numero = numero.replaceAll(',', '.');
+          // Obtém a largura da tela e atualiza o número máximo de dígitos.
+          maxDigits = MediaQuery.of(context).size.width ~/ 40;
+          // Se o número exceder o número máximo de dígitos, remove os dígitos extras do final.
+          if (numero.length > maxDigits) {
+            numero = numero.substring(0, maxDigits);
+          }
         });
         break;
       case '+':
@@ -95,73 +106,75 @@ class _CalculadoraState extends State<Calculadora> {
     }
   }
 
-
-
+  // Função para construir a interface do usuário do aplicativo.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text('Calculadora'),
+        appBar: AppBar(
+          title: Center(
+            child: Text('Calculadora'), // Título da aplicação.
+          ),
+          backgroundColor: Colors.indigo,
         ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text(numero, style: TextStyle(fontSize: 70),),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              GestureDetector(onTap: () => calcular('AC'),child: Text('AC', style: TextStyle(fontSize: 48),)),
-              Text(' ', style: TextStyle(fontSize: 48),),
-              Text(' ', style: TextStyle(fontSize: 48),),
-              GestureDetector(onTap: () => calcular('<X'),child: Text('<X', style: TextStyle(fontSize: 48),)),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              GestureDetector(onTap: () => calcular('7'),child: Text('7', style: TextStyle(fontSize: 48),)),
-              GestureDetector(onTap: () => calcular('8'),child: Text('8', style: TextStyle(fontSize: 48),)),
-              GestureDetector(onTap: () => calcular('9'),child: Text('9', style: TextStyle(fontSize: 48),)),
-              GestureDetector(onTap: () => calcular('/'),child: Text('/', style: TextStyle(fontSize: 48),)),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              GestureDetector(onTap: () => calcular('4'),child: Text('4', style: TextStyle(fontSize: 48),)),
-              GestureDetector(onTap: () => calcular('5'),child: Text('5', style: TextStyle(fontSize: 48),)),
-              GestureDetector(onTap: () => calcular('6'),child: Text('6', style: TextStyle(fontSize: 48),)),
-              GestureDetector(onTap: () => calcular('X'),child: Text('X', style: TextStyle(fontSize: 48),)),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              GestureDetector(onTap: () => calcular('1'),child: Text('1', style: TextStyle(fontSize: 48),)),
-              GestureDetector(onTap: () => calcular('2'),child: Text('2', style: TextStyle(fontSize: 48),)),
-              GestureDetector(onTap: () => calcular('3'),child: Text('3', style: TextStyle(fontSize: 48),)),
-              GestureDetector(onTap: () => calcular('-'),child: Text('-', style: TextStyle(fontSize: 48),)),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              GestureDetector(onTap: () => calcular('0'),child: Text('0', style: TextStyle(fontSize: 48),)),
-              GestureDetector(onTap: () => calcular(','),child: Text(',', style: TextStyle(fontSize: 48),)),
-              GestureDetector(onTap: () => calcular('='),child: Text('=', style: TextStyle(fontSize: 48),)),
-              GestureDetector(onTap: () => calcular('+'),child: Text('+', style: TextStyle(fontSize: 48),)),
-            ],
-          ),
-        ],
-      )
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Text(numero, style: TextStyle(fontSize: 70, color: Colors.indigo),), // Exibe o número atual.
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(onTap: () => calcular('AC'),child: Text('AC', style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold, color: Colors.indigo),)),
+                Text(' ', style: TextStyle(fontSize: 48),),
+                Text(' ', style: TextStyle(fontSize: 48),),
+                GestureDetector(onTap: () => calcular('<X'),child: Image.asset('assets/images/apagar.png', width: 60,)),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(onTap: () => calcular('7'),child: Text('7', style: TextStyle(fontSize: 48),)),
+                GestureDetector(onTap: () => calcular('8'),child: Text('8', style: TextStyle(fontSize: 48),)),
+                GestureDetector(onTap: () => calcular('9'),child: Text('9', style: TextStyle(fontSize: 48),)),
+                GestureDetector(onTap: () => calcular('/'),child: Image.asset('assets/images/divisao.png', width: 50,)),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(onTap: () => calcular('4'),child: Text('4', style: TextStyle(fontSize: 48),)),
+                GestureDetector(onTap: () => calcular('5'),child: Text('5', style: TextStyle(fontSize: 48),)),
+                GestureDetector(onTap: () => calcular('6'),child: Text('6', style: TextStyle(fontSize: 48),)),
+                GestureDetector(onTap: () => calcular('X'),child: Image.asset('assets/images/multiplicacao.png', width: 50,)),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(onTap: () => calcular('1'),child: Text('1', style: TextStyle(fontSize: 48),)),
+                GestureDetector(onTap: () => calcular('2'),child: Text('2', style: TextStyle(fontSize: 48),)),
+                GestureDetector(onTap: () => calcular('3'),child: Text('3', style: TextStyle(fontSize: 48),)),
+                GestureDetector(onTap: () => calcular('-'),child: Image.asset('assets/images/subtracao.png', width: 50,)),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(onTap: () => calcular('0'),child: Text('0', style: TextStyle(fontSize: 48),)),
+                GestureDetector(onTap: () => calcular(','),child: Text(',', style: TextStyle(fontSize: 48),)),
+                GestureDetector(onTap: () => calcular('='),child: Image.asset('assets/images/igual.png', width: 50,)),
+                GestureDetector(onTap: () => calcular('+'),child: Image.asset('assets/images/soma.png', width: 90,)),
+              ],
+            ),
+          ],
+        )
     );
   }
 }
-  
